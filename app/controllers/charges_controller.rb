f9d8c469c2caf2eb108a5840360ff96b6cf2efaa
Kevin Bellinger <kevinb@outlook.com>
2014-07-29 10:54:52 +0000

    finished stripe signup and writeback to db

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,13 +5,17 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = 500
+    # @amount = 500
 
     customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
+      :email => current_user.email,
       :plan => "Premium",
       :card  => params[:stripeToken]
     )
+
+    #adding writeback to customer account for access.
+    current_user.update_attribute(:role, "premium")
+    redirect_to wikis_path, flash: { notice: "You are now subscribed to Blocipedia!" }
 
   #  charge = Stripe::Charge.create(
    #   :customer    => customer.id,
